c97648e13ab65019490137890fc866e2b1f8f328
Rahul Gopinath <rahul@gopinath.org>
2016-08-10 00:51:52 -0700

    update

--- public/main.js
+++ public/main.js
@@ -1,11 +1,31 @@
 for (let v of document.getElementsByClassName("update")) {
   v.addEventListener("click", function(event) {
     var req = new XMLHttpRequest();
-    var payload = {name:null, reps:null, weight:null, date:null, lbs:null};
+    var payload = {key:mykey, id:this.value};
     alert(this.id);
-    //req.open("POST", "/insert", true);
-    //req.setRequestHeader("Content-Tpye", "application/json");
-    //req.send(JSON.stringify(payload));
+    req.open("POST", "/update", true);
+    req.setRequestHeader("Content-Tpye", "application/json");
+    req.send(JSON.stringify(payload));
     event.preventDefault();
   });
 }
+
+for (let v of document.getElementsByClassName("remove")) {
+  v.addEventListener("click", function(event) {
+    var req = new XMLHttpRequest();
+    var payload = {key: mykey, id:this.value}
+    req.open("POST", "/remove", true);
+    req.setRequestHeader("Content-Tpye", "application/json");
+    req.send(JSON.stringify(payload));
+    event.preventDefault();
+  });
+}
+
+document.getElementsById("add").addEventListener("click", function(event) {
+  var req = new XMLHttpRequest();
+  var payload = {key:mykey, link:mylink, text:mytext};
+  req.open("POST", "/insert", true);
+  req.setRequestHeader("Content-Tpye", "application/json");
+  req.send(JSON.stringify(payload));
+  event.preventDefault();
+});

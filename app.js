5b6fd066e9dda249e2566f88e5789f4e8f6d47de
Rahul Gopinath <rahul@gopinath.org>
2016-08-09 22:56:30 -0700

    update

--- app.js
+++ app.js
@@ -12,17 +12,12 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
+app.use(express.static('public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res, next) {
   var context = {}
-  //context.rows = [
-  //  {"link" : "/", "text" : "Home"},
-  //  {"link" : "http://www.google.com", "text" : "google"},
-  //];
   pg.pool.query('SELECT * from links', function(err, result) {
     console.log(result.rows[0]); // output: foo
     context.rows = result.rows;
@@ -30,7 +25,13 @@ app.get('/', function(req, res, next) {
 });
 });
 
-app.get('/reset', function(req, res, next) {
+app.post('/insert', function(req, res, next) {
+  console.log(req.body);
+  var key = req.body.key;
+  pg.pool.query('SELECT * from links', function(err, result) {
+    rows = JSON.stringify(result);
+    res.send(rows);
+  }
 });
 
 app.listen(app.get('port'), function() { console.log('Scratch is running on port', app.get('port')); });
